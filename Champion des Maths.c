d5366ebd6edcd2b178c4a89ab32262447523c833
Tealyx <153727012+Tealyx@users.noreply.github.com>
2026-02-14 16:19:05 +0000

    Ajout de commentaire, structure, et table de multi

--- Champion des Maths.c
+++ Champion des Maths.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+/* --------------------| Fonctions des Affichages des choix de séléction des jeux ainsi que les choix concernés |-------------------- */
+
 void affichechoix() {
     printf("+-------------------------------+\n");
     printf("|1 : Addition                   |\n");
@@ -33,7 +35,8 @@ void choix() {
             break;
 
         case 4:
-            printf("Tables de multiplications\n");
+            affichechoixtable(); // Autre choix dans ce cas la
+            choixtable();
             break;
 
         case 5:
@@ -49,8 +52,43 @@ void choix() {
     }
 }
 
+/* --------------------| Fonctions des Affichages des choix des tables de multiplication ainsi que les choix concernés |-------------------- */
 
-int addition() {
+void affichechoixtable() {
+    printf("+------------------------------------------------+\n");
+    printf("|1 : Afficher une table de multiplication        |\n");
+    printf("|2 : S'entraîner sur une table de multiplication |\n");
+    printf("|0 : Sortir du jeu                               |\n");
+    printf("+------------------------------------------------+\n");
+    printf("Quel est votre choix ?\n");
+}
+
+void choixtable() {
+    int choix = 0;
+
+    scanf("%d", &choix);
+
+    switch (choix) {
+        case 1:
+            affichetable();
+            break;
+
+        case 2:
+            jeuxtable();
+            break;
+
+        case 0:
+            printf("Merci de votre visite.\n");
+            break;
+
+        default:
+            printf("Choix invalide.\n");
+    }
+}
+
+/* --------------------| Fonctions de l'addition |-------------------- */
+
+void addition() {
     int user_rep = 0, rep = 0;
     
     int a = rand() % 101;
@@ -72,7 +110,9 @@ int addition() {
     }
 }
 
-int multiplication() {
+/* --------------------| Fonctions de la multiplication |-------------------- */
+
+void multiplication() {
     int user_rep = 0, rep = 0;
     
     int a = rand() % 11;
@@ -94,7 +134,9 @@ int multiplication() {
     }
 }
 
-int soustraction() {
+/* --------------------| Fonction de la soustraction |-------------------- */
+
+void soustraction() {
     int user_rep = 0, rep = 0, temp = 0;
     
     int a = rand() % 101;
@@ -121,6 +163,47 @@ int soustraction() {
         printf("La réponse était %d", rep);
     }
 }
+
+/* --------------------| Fonctions des tables de multiplication |-------------------- */
+
+// Choix 1 : Affiche la table de l'utilisateur
+void affichetable() {
+    int i, n;
+
+    printf("Quelle table de multiplication souhaitez-vous afficher ?\n");
+    scanf("%d", &n);
+
+    for (i = 0; i < 11; i++)
+    {
+        printf("%d * %d = %d\n", n, i, n * i);
+    }
+    
+}
+
+// Choix 2 : L'utilisateur s'exerce sur la table de son choix
+void jeuxtable() {
+    int i, n, rep;
+
+    printf("Sur quel table de multiplication souhaitez-vous vous exercez ?\n");
+    scanf("%d", &n);
+
+    for (i = 0; i < 11; i++)
+    {
+        printf("%d * %d = ", n, i);
+        scanf("%d", &rep);
+
+        if (rep == n * i)
+        {
+            printf("Gagné !\n");
+        }
+        else {
+            printf("Perdu...\n");
+        } 
+    }
+
+}
+
+/* --------------------| Fonction principal |-------------------- */
 
 int main(void) {
     srand(time(NULL));
