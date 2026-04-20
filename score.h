c09c53c4088b6504d2cf702065824d7e23b790a2
Tealyx <yanicewroblewski@gmail.com>
2026-04-20 16:32:06 +0200

    Delete score.h

--- score.h
+++
@@ -1,9 +0,0 @@
-#ifndef SCORE_H
-#define SCORE_H
-
-char* demander_nom();
-int lire_point(char *nom);
-void ajouter_point(char *nom, int score);
-void calcul_points(int compteur, char *nom, bool est_correct, int rep);
-
-#endif // SCORE_H
