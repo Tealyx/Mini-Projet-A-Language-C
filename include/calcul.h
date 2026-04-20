82e0ab2f3e45e74505c576d308da44c63af68a63
Tealyx <yanicewroblewski@gmail.com>
2026-04-20 16:32:35 +0200

    Add files via upload

---
+++ include/calcul.h
@@ -0,0 +1,12 @@
+#ifndef CALCUL_H
+#define CALCUL_H
+
+void addition(char *nom);
+void soustraction(char *nom);
+void multiplication(char *nom);
+void division(char *nom);
+
+void affichetable();
+void jeuxtable(char *nom);
+
+#endif // CALCUL_H
---
+++ include/menu.h
@@ -0,0 +1,11 @@
+#ifndef MENU_H
+#define MENU_H
+
+char* demander_nom();
+void affichechoix(char *nom);
+void choix(char *nom);
+
+void affichechoixtable();
+void choixtable(char *nom);
+
+#endif // MENU_H
